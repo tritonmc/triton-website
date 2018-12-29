faad34e13f19c4fd365211c8389c3a10bf8da85c
Diogo Correia <me@diogotc.com>
2018-12-29 20:55:54 +0000

    Updated GitHub redirect URLs since project URL has changed.

--- backend/redirect.js
+++ backend/redirect.js
@@ -1,12 +1,11 @@
 // Setup redirects here
 const redirectMap = {
   "/discord": "https://discord.gg/9FsawxR",
-  "/github": "https://github.com/Rexcantor/MultiLanguagePlugin/",
-  "/github_issues": "https://github.com/Rexcantor/MultiLanguagePlugin/issues",
-  "/github_config": "https://github.com/Rexcantor/MultiLanguagePlugin/blob/master/config.yml",
-  "/github_config_bungee":
-    "https://github.com/Rexcantor/MultiLanguagePlugin/blob/master/bungee_config.yml",
-  "/api": "https://github.com/Rexcantor/MultiLanguagePlugin/wiki",
+  "/github": "https://github.com/Rexcantor/Triton/",
+  "/github_issues": "https://github.com/Rexcantor/Triton/issues",
+  "/github_config": "https://github.com/Rexcantor/Triton/blob/master/config.yml",
+  "/github_config_bungee": "https://github.com/Rexcantor/Triton/blob/master/bungee_config.yml",
+  "/api": "https://github.com/Rexcantor/Triton/wiki",
   "/spigot": "https://www.spigotmc.org/resources/triton.30331/",
   "/api-latest": "https://cdn.rexcantor64.com/triton/api/TritonAPI-v1.0.0.jar",
 };
