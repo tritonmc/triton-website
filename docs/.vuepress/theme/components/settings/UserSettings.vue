c8fbf44d06264ae5e459df6c1b4d70fd2f1ba2e8
Diogo Correia <me@diogotc.com>
2020-06-21 20:11:52 +0100

    Swapped dark mode menu for 1-click toggle
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- docs/.vuepress/theme/components/settings/UserSettings.vue
+++ docs/.vuepress/theme/components/settings/UserSettings.vue
@@ -1,48 +1,52 @@
 <template>
-	<div v-if="showSettings" v-click-outside="hideMenu" class="user-settings">
-		<a class="settings-button" href="#" @click.prevent="showMenu = !showMenu">
-			<CogIcon class="settings-icon" />
-		</a>
-		<transition name="menu-transition" mode="out-in">
-			<div v-show="showMenu" class="user-settings-menu">
-				<ThemeOptions />
-			</div>
-		</transition>
-	</div>
+  <div v-if="showSettings" v-click-outside="hideMenu" class="user-settings">
+    <a class="settings-button" href="#" @click.prevent="onToggleClick">
+      <MoonIcon v-if="darkTheme" class="settings-icon" />
+      <LightbulbIcon v-else class="settings-icon" />
+    </a>
+    <transition name="menu-transition" mode="out-in">
+      <div v-show="showMenu" class="user-settings-menu">
+        <ThemeOptions />
+      </div>
+    </transition>
+  </div>
 </template>
 
 <script>
 import ClickOutside from 'vue-click-outside';
 import ThemeOptions from '@theme/components/settings/ThemeOptions.vue';
-import CogIcon from '@theme/components/settings/CogIcon.vue';
+import MoonIcon from '@theme/components/settings/MoonIcon.vue';
+import LightbulbIcon from '@theme/components/settings/LightbulbIcon.vue';
 import yuuConfig from '@theme/mixins/yuuConfig.js';
+import darkThemeHandler from '@theme/mixins/darkThemeHandler.js';
 
 export default {
-	name: 'UserSettings',
-	directives: {
-		'click-outside': ClickOutside,
-	},
-	components: {
-		CogIcon,
-		ThemeOptions,
-	},
-	mixins: [yuuConfig],
-	data() {
-		return {
-			showMenu: false,
-		};
-	},
-	computed: {
-		showSettings() {
-			const { yuu } = this;
-			return yuu.hasThemes || yuu.disableDarkTheme !== true || yuu.disableThemeIgnore !== true;
-		},
-	},
-	methods: {
-		hideMenu() {
-			this.showMenu = false;
-		},
-	},
+  name: 'UserSettings',
+  directives: {
+    'click-outside': ClickOutside,
+  },
+  components: {
+    MoonIcon,
+    LightbulbIcon,
+    ThemeOptions,
+  },
+  mixins: [yuuConfig, darkThemeHandler],
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  computed: {
+    showSettings() {
+      const { yuu } = this;
+      return yuu.hasThemes || yuu.disableDarkTheme !== true || yuu.disableThemeIgnore !== true;
+    },
+  },
+  methods: {
+    hideMenu() {
+      this.showMenu = false;
+    },
+  },
 };
 </script>
 
@@ -50,85 +54,83 @@ export default {
 @import '../../styles/variables.styl';
 
 .user-settings {
-	position: relative;
-	margin-right: 1em;
+  position: relative;
+  margin-right: 1em;
 
-	.settings-button {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		height: 100%;
+  .settings-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
 
-		.settings-icon {
-			width: 18px;
-		}
-	}
+    .settings-icon {
+      width: 18px;
+    }
+  }
 
-	.user-settings-menu {
-		background-color: #fff;
-		position: absolute;
-		top: 40px;
-		left: 50%;
-		min-width: 100px;
-		margin: 0;
-		padding: 1em;
-		border: 1px solid $borderColor;
-		border-radius: 4px;
-		transform: translateX(-50%);
-		z-index: 150;
+  .user-settings-menu {
+    background-color: #fff;
+    position: absolute;
+    top: 40px;
+    left: 50%;
+    min-width: 100px;
+    margin: 0;
+    padding: 1em;
+    border: 1px solid $borderColor;
+    border-radius: 4px;
+    transform: translateX(-50%);
+    z-index: 150;
 
-		&::before {
-			content: '';
-			position: absolute;
-			top: -7px;
-			left: 50%;
-			border-style: solid;
-			border-color: transparent transparent $borderColor;
-			border-width: 0 7px 7px;
-			transform: translateX(-50%);
-		}
+    &::before {
+      content: '';
+      position: absolute;
+      top: -7px;
+      left: 50%;
+      border-style: solid;
+      border-color: transparent transparent $borderColor;
+      border-width: 0 7px 7px;
+      transform: translateX(-50%);
+    }
 
-		&.menu-transition-enter-active,
-		&.menu-transition-leave-active {
-			transition: all 0.25s ease-in-out;
-		}
+    &.menu-transition-enter-active, &.menu-transition-leave-active {
+      transition: all 0.25s ease-in-out;
+    }
 
-		&.menu-transition-enter,
-		&.menu-transition-leave-to {
-			top: 50px;
-			opacity: 0;
-		}
+    &.menu-transition-enter, &.menu-transition-leave-to {
+      top: 50px;
+      opacity: 0;
+    }
 
-		ul {
-			list-style-type: none;
-			margin: 0;
-			padding: 0;
-		}
-	}
+    ul {
+      list-style-type: none;
+      margin: 0;
+      padding: 0;
+    }
+  }
 }
 
 .yuu-theme-dark {
-	.user-settings-menu {
-		background-color: $darkPrimaryBg;
-		border-color: $darkBorderColor;
+  .user-settings-menu {
+    background-color: $darkPrimaryBg;
+    border-color: $darkBorderColor;
 
-		&::before {
-			border-bottom-color: $darkBorderColor;
-		}
-	}
+    &::before {
+      border-bottom-color: $darkBorderColor;
+    }
+  }
 }
 
 @media (max-width: $MQMobile) {
-	.user-settings {
-		margin-right: 0;
+  .user-settings {
+    margin-right: 0;
 
-		.user-settings-menu {
-			left: calc(50% - 35px);
+    .user-settings-menu {
+      left: calc(50% - 35px);
 
-			&::before {
-				left: calc(50% + 35px);
-			}
-		}
-	}
+      &::before {
+        left: calc(50% + 35px);
+      }
+    }
+  }
 }
 </style>
--- docs/.vuepress/theme/mixins/darkThemeHandler.js
+++ docs/.vuepress/theme/mixins/darkThemeHandler.js
@@ -1,44 +1,50 @@
 export default {
-	data() {
-		return {
-			darkTheme: false,
-			ignoreForcedThemes: false,
-		};
-	},
+  data() {
+    return {
+      darkTheme: false,
+      ignoreForcedThemes: false,
+    };
+  },
 
-	mounted() {
-		if (this.yuu.disableDarkTheme !== true) {
-			if (this.yuu.defaultDarkTheme === true && !localStorage.getItem('dark-theme')) {
-				localStorage.setItem('dark-theme', true);
-			}
+  mounted() {
+    if (this.yuu.disableDarkTheme !== true) {
+      if (this.yuu.defaultDarkTheme === true && !localStorage.getItem('dark-theme')) {
+        localStorage.setItem('dark-theme', true);
+      }
 
-			this.darkTheme = localStorage.getItem('dark-theme') === 'true';
-			this.toggleDarkTheme();
-		}
+      this.darkTheme = localStorage.getItem('dark-theme') === 'true';
+      this.toggleDarkTheme();
+    }
 
-		if (this.yuu.disableThemeIgnore !== true) {
-			this.ignoreForcedThemes = localStorage.getItem('ignore-forced-themes') === 'true';
-		}
-	},
+    if (this.yuu.disableThemeIgnore !== true) {
+      this.ignoreForcedThemes = localStorage.getItem('ignore-forced-themes') === 'true';
+    }
+  },
 
-	methods: {
-		toggleDarkTheme() {
-			if (this.darkTheme) {
-				document.body.classList.add('yuu-theme-dark');
-				return localStorage.setItem('dark-theme', true);
-			}
+  methods: {
+    onToggleClick() {
+      this.darkTheme = !this.darkTheme;
+      this.toggleDarkTheme();
+    },
 
-			document.body.classList.remove('yuu-theme-dark');
-			localStorage.setItem('dark-theme', false);
-		},
+    toggleDarkTheme() {
+      console.log('clicked');
+      if (!this.darkTheme) {
+        document.body.classList.add('yuu-theme-dark');
+        return localStorage.setItem('dark-theme', true);
+      }
 
-		toggleForcedThemes() {
-			if (this.ignoreForcedThemes) {
-				this.setTheme(localStorage.getItem('color-theme'));
-				return localStorage.setItem('ignore-forced-themes', true);
-			}
+      document.body.classList.remove('yuu-theme-dark');
+      localStorage.setItem('dark-theme', false);
+    },
 
-			localStorage.removeItem('ignore-forced-themes');
-		},
-	},
+    toggleForcedThemes() {
+      if (this.ignoreForcedThemes) {
+        this.setTheme(localStorage.getItem('color-theme'));
+        return localStorage.setItem('ignore-forced-themes', true);
+      }
+
+      localStorage.removeItem('ignore-forced-themes');
+    },
+  },
 };
