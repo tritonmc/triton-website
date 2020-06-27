3a352f4e652e96bd48d909545983a612c16bdb4d
Diogo Correia <me@diogotc.com>
2020-06-27 23:44:29 +0100

    Dark mode toggle simplify (#12)

--- docs/.vuepress/theme/mixins/darkThemeHandler.js
+++ docs/.vuepress/theme/mixins/darkThemeHandler.js
@@ -1,44 +1,36 @@
 export default {
-	data() {
-		return {
-			darkTheme: false,
-			ignoreForcedThemes: false,
-		};
-	},
+  data() {
+    return {
+      darkTheme: false,
+    };
+  },
 
-	mounted() {
-		if (this.yuu.disableDarkTheme !== true) {
-			if (this.yuu.defaultDarkTheme === true && !localStorage.getItem('dark-theme')) {
-				localStorage.setItem('dark-theme', true);
-			}
+  mounted() {
+    if (this.yuu.disableDarkTheme !== true) {
+      if (this.yuu.defaultDarkTheme === true && !localStorage.getItem('dark-theme')) {
+        const { matches } = window.matchMedia('(prefers-color-scheme: dark)');
+        localStorage.setItem('dark-theme', matches);
+      }
 
-			this.darkTheme = localStorage.getItem('dark-theme') === 'true';
-			this.toggleDarkTheme();
-		}
+      this.darkTheme = localStorage.getItem('dark-theme') === 'true';
+      this.toggleDarkTheme();
+    }
+  },
 
-		if (this.yuu.disableThemeIgnore !== true) {
-			this.ignoreForcedThemes = localStorage.getItem('ignore-forced-themes') === 'true';
-		}
-	},
+  methods: {
+    onToggleClick() {
+      this.darkTheme = !this.darkTheme;
+      this.toggleDarkTheme();
+    },
 
-	methods: {
-		toggleDarkTheme() {
-			if (this.darkTheme) {
-				document.body.classList.add('yuu-theme-dark');
-				return localStorage.setItem('dark-theme', true);
-			}
+    toggleDarkTheme() {
+      if (this.darkTheme) {
+        document.body.classList.add('yuu-theme-dark');
+        return localStorage.setItem('dark-theme', true);
+      }
 
-			document.body.classList.remove('yuu-theme-dark');
-			localStorage.setItem('dark-theme', false);
-		},
-
-		toggleForcedThemes() {
-			if (this.ignoreForcedThemes) {
-				this.setTheme(localStorage.getItem('color-theme'));
-				return localStorage.setItem('ignore-forced-themes', true);
-			}
-
-			localStorage.removeItem('ignore-forced-themes');
-		},
-	},
+      document.body.classList.remove('yuu-theme-dark');
+      localStorage.setItem('dark-theme', false);
+    },
+  },
 };
