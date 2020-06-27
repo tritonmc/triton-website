5fc0ddafabf5fc8ea930819d0866676f8a9b8137
Diogo Correia <me@diogotc.com>
2020-06-27 23:37:38 +0100

    Remove unused code
    Signed-off-by: Diogo Correia <me@diogotc.com>

--- docs/.vuepress/theme/components/settings/UserSettings.vue
+++ docs/.vuepress/theme/components/settings/UserSettings.vue
@@ -56,70 +56,11 @@ export default {
       width: 18px;
     }
   }
-
-  .user-settings-menu {
-    background-color: #fff;
-    position: absolute;
-    top: 40px;
-    left: 50%;
-    min-width: 100px;
-    margin: 0;
-    padding: 1em;
-    border: 1px solid $borderColor;
-    border-radius: 4px;
-    transform: translateX(-50%);
-    z-index: 150;
-
-    &::before {
-      content: '';
-      position: absolute;
-      top: -7px;
-      left: 50%;
-      border-style: solid;
-      border-color: transparent transparent $borderColor;
-      border-width: 0 7px 7px;
-      transform: translateX(-50%);
-    }
-
-    &.menu-transition-enter-active, &.menu-transition-leave-active {
-      transition: all 0.25s ease-in-out;
-    }
-
-    &.menu-transition-enter, &.menu-transition-leave-to {
-      top: 50px;
-      opacity: 0;
-    }
-
-    ul {
-      list-style-type: none;
-      margin: 0;
-      padding: 0;
-    }
-  }
-}
-
-.yuu-theme-dark {
-  .user-settings-menu {
-    background-color: $darkPrimaryBg;
-    border-color: $darkBorderColor;
-
-    &::before {
-      border-bottom-color: $darkBorderColor;
-    }
-  }
 }
 
 @media (max-width: $MQMobile) {
   .user-settings {
     margin-right: 0;
-
-    .user-settings-menu {
-      left: calc(50% - 35px);
-
-      &::before {
-        left: calc(50% + 35px);
-      }
-    }
   }
 }
 </style>
